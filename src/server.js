81a702f040d25f8df641556becf686fadaf5addc
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-02-17 12:28:46 +0000

    cloned medium backend

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const articleRoute = require("./articles");
+const authorRoute = require("./authors");
+
+const server = express();
+const port = process.env.PORT;
+
+server.use(cors());
+server.use(express.json());
+server.use("/articles", articleRoute);
+server.use("/authors", authorRoute);
+
+mongoose
+  .connect(process.env.MONGO_ATLAS, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    server.listen(port, () => {
+      console.log("The server's power level is over ", port);
+    })
+  );
